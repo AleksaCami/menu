9885ce608d430ac47d08263ce14db6d1f9f57fb6
Aleksa Camilovic <aleksa@utlimatemovers.net>
2020-02-29 00:10:37 +0100

    Lint fix files

--- src/main.js
+++ src/main.js
@@ -1,16 +1,21 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import Autocomplete from 'v-autocomplete';
+import Autocomplete from "v-autocomplete";
 
 // You need a specific loader for CSS files like https://github.com/webpack/css-loader
-import 'v-autocomplete/dist/v-autocomplete.css';
+import "v-autocomplete/dist/v-autocomplete.css";
 
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faSearch, faEuroSign, faPlusCircle, faTrash } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSearch,
+  faEuroSign,
+  faPlusCircle,
+  faTrash
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 Vue.config.productionTip = false;
