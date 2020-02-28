1cc0e3fb021d8c661c245e443840c8b1f80aa510
Aleksa Camilovic <aleksa@utlimatemovers.net>
2020-02-28 15:24:17 +0100

    Complete header

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,10 +1,19 @@
 <template>
-  <div class="flex fixed">
-    <div class="nav-color nav-content-left">
-      <span>Menu left</span>
+  <div class="nav-color nav-container fixed col-12 pl-0">
+    <div class="col-4 nav-color-left nav-content-left">
+      <img
+        class="logo"
+        src="../../assets/images/menulogo500x500px-01.png"
+        alt="menu logo"
+        height="70"
+      />
     </div>
-    <div class="nav-color nav-content-right">
-      <span>Menu right</span>
+    <div class="col-8 nav-content-right">
+      <i class="fa fa-eur" aria-hidden="true" />
+      <font-awesome-icon icon="euro-sign" style="color: white" />
+      <span class="nav-text">
+        Currencies
+      </span>
     </div>
   </div>
 </template>
@@ -13,8 +22,9 @@
 $white: #fff;
 $accent: #ff6600;
 
-.flex {
+.nav-container {
   display: flex;
+  min-width: 1200px;
 }
 .fixed {
   overflow: hidden;
@@ -22,15 +32,22 @@ $accent: #ff6600;
   top: 0;
   width: 100%;
   z-index: 1000;
-  margin-left: -8px;
 }
 .nav-text {
   color: $white;
+  padding-left: 10px;
+  font-size: 14px;
+  font-weight: 600;
 }
 .nav-content-left {
-  padding: 15px;
+  background-color: #e16320;
+  display: flex;
+  justify-content: flex-start;
 }
 .nav-content-right {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
   padding: 15px;
 }
 .nav-color {
